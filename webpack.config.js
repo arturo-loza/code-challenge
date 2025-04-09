0e90bfb26164c304c46a71ba01f39b21a88c5c1a
Arturo Loza <arturo.loza@coderoad.com>
2025-04-08 20:33:52 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const package = require("./package.json");
+const commonPaths = require("./build_utils/config/commonPaths");
+
+const isDebug = !process.argv.includes("release");
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    entry: commonPaths.entryPath,
+    output: {
+        uniqueName: package.name,
+        publicPath: "/",
+        path: commonPaths.outputPath,
+        filename: `${package.version}/js/[name].[chunkhash:8].js`,
+        chunkFilename: `${package.version}/js/[name].[chunkhash:8].js`,
+        assetModuleFilename: isDebug
+            ? `images/[path][name].[contenthash:8][ext]`
+            : `images/[path][contenthash:8][ext]`,
+        crossOriginLoading: "anonymous",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html",
+            filename: "index.html",
+        }),
+    ],
+    devServer: {
+        port: port,
+        static: {
+            directory: commonPaths.outputPath,
+        },
+        historyApiFallback: {
+            index: "index.html",
+        },
+        webSocketServer: false,
+    },
+};
